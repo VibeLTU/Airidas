1e0a4f591eaa450cd2af291b00d85bfabffcbdc9
VibeLTU <147432406+VibeLTU@users.noreply.github.com>
2024-02-07 17:28:08 +0200

    Pridejau pirmos uzd faila
    Siame faile teisingai apskaiciuojamas vidurkis be teisingai isvedama ivesta informacija. Taciau dar reiks optimizuoti koda ir apsaiciuoti mediana.

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+struct duomenys {
+  string vardas;
+  string pavarde;
+  int nd; // namu darbu rezultatai
+  int eg; // egzamino rezultatai
+  double ndvid; // namu darbu vidurkis
+  double galutinis; // bendras vidurkis
+};
+
+int main() {
+  duomenys A[50];
+  int n, m;
+  cout << "Iveskite studentu skaiciu: ";
+  cin >> m;
+  cout << "Iveskite namu darbu skaiciu: ";
+  cin >> n;
+  
+  for (int i = 0; i < m ; i++) {
+    cout << "Iveskite varda: " << endl;
+    cin >> A[i].vardas;
+    cout << "Iveskite pavarde: " << endl;
+    cin >> A[i].pavarde;
+    for (int j=0; j<n; j++){
+      cout << "Iveskite namu darbu rezultatus: " << endl;
+      cin >> A[i].nd;
+    }
+    cout << "Iveskite egzamino rezultatus: " << endl;
+    cin >> A[i].eg;
+  }
+  for (int i = 0; i < m ; i++){//accumulate panaudoti, kad butu greiciau veliau skaiciuoti vidurki
+    A[i].ndvid = 0;
+    for(int j=0; j<n; j++){
+      A[i].ndvid = A[i].ndvid + A[j].nd;
+    }
+    A[i].ndvid = A[i].ndvid / n;
+    A[i].galutinis = 0.4 * A[i].ndvid + 0.6 * A[i].eg;
+  }
+  cout << "Vardas" << "         " << "Pavarde" << "        " << "Galutinis" << endl;
+  cout << "--------------------------------------" << endl;
+  for (int i = 0; i < m ; i++){
+  cout << left << setw(15) << A[i].vardas << left << setw(15) << A[i].pavarde << left << setw(15) << setprecision(3) << A[i].galutinis << endl;
+}
+}
