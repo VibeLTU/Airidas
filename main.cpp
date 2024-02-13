4542eb3e365437858ee35f454526ea83c17a13a8
VibeLTU <147432406+VibeLTU@users.noreply.github.com>
2024-02-14 00:11:17 +0200

    Pataisytas biski kodas
    Kazkodel blogai skaiciavo butent pati pirma vidurki, tad sutaisiau koda. Dabar reikia apskaiciuoti mediana.

--- main.cpp
+++ main.cpp
@@ -4,45 +4,41 @@
 using namespace std;
 
 struct duomenys {
-  string vardas;
-  string pavarde;
-  int nd; // namu darbu rezultatai
-  int eg; // egzamino rezultatai
-  double ndvid; // namu darbu vidurkis
-  double galutinis; // bendras vidurkis
+    string vardas;
+    string pavarde;
+    double nd; // namu darbu rezultatai
+    double eg; // egzamino rezultatai
+    double ndvid; // namu darbu vidurkis
+    double galutinis; // bendras vidurkis
 };
 
 int main() {
-  duomenys A[50];
-  int n, m;
-  cout << "Iveskite studentu skaiciu: ";
-  cin >> m;
-  cout << "Iveskite namu darbu skaiciu: ";
-  cin >> n;
-  
-  for (int i = 0; i < m ; i++) {
-    cout << "Iveskite varda: " << endl;
-    cin >> A[i].vardas;
-    cout << "Iveskite pavarde: " << endl;
-    cin >> A[i].pavarde;
-    for (int j=0; j<n; j++){
-      cout << "Iveskite namu darbu rezultatus: " << endl;
-      cin >> A[i].nd;
+    duomenys A[50];
+    int n, m;
+    cout << "Iveskite studentu skaiciu: ";
+    cin >> m;
+    cout << "Iveskite namu darbu skaiciu: ";
+    cin >> n;
+
+    for (int i = 0; i < m; i++) {
+        A[i].ndvid = 0;
+        cout << "Iveskite varda: " << endl;
+        cin >> A[i].vardas;
+        cout << "Iveskite pavarde: " << endl;
+        cin >> A[i].pavarde;
+        for (int j = 0; j < n; j++) {
+            cout << "Iveskite namu darbu rezultatus: " << endl;
+            cin >> A[i].nd;
+            A[i].ndvid += A[i].nd;
+        }
+        A[i].ndvid = A[i].ndvid / n;
+        cout << "Iveskite egzamino rezultatus: " << endl;
+        cin >> A[i].eg;
+        A[i].galutinis = 0.4 * A[i].ndvid + 0.6 * A[i].eg;
     }
-    cout << "Iveskite egzamino rezultatus: " << endl;
-    cin >> A[i].eg;
-  }
-  for (int i = 0; i < m ; i++){//accumulate panaudoti, kad butu greiciau veliau skaiciuoti vidurki
-    A[i].ndvid = 0;
-    for(int j=0; j<n; j++){
-      A[i].ndvid = A[i].ndvid + A[j].nd;
+    cout << "Vardas" << "         " << "Pavarde" << "        " << "Galutinis (Vid.)" << endl;
+    cout << "--------------------------------------" << endl;
+    for (int i = 0; i < m; i++) {
+        cout << left << setw(15) << A[i].vardas << left << setw(15) << A[i].pavarde << left << setw(15) << setprecision(3) << A[i].galutinis << endl;
     }
-    A[i].ndvid = A[i].ndvid / n;
-    A[i].galutinis = 0.4 * A[i].ndvid + 0.6 * A[i].eg;
-  }
-  cout << "Vardas" << "         " << "Pavarde" << "        " << "Galutinis" << endl;
-  cout << "--------------------------------------" << endl;
-  for (int i = 0; i < m ; i++){
-  cout << left << setw(15) << A[i].vardas << left << setw(15) << A[i].pavarde << left << setw(15) << setprecision(3) << A[i].galutinis << endl;
-}
 }
