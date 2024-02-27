9b9f29def58a189991c5d843fe7923b5a9202b32
VibeLTU <147432406+VibeLTU@users.noreply.github.com>
2024-02-27 21:07:01 +0200

    Update Struktura.h
    Perkeliau strukturas ir kintamuosius

--- Struktura.h
+++ Struktura.h
@@ -1,1 +1,27 @@
+#ifndef STRUKTURA_H_INCLUDED
+#define STRUKTURA_H_INCLUDED
+
+#include <string>
+#include <vector>
+
+struct Duomenys {
+    std::string vardas;
+    std::string pavarde;
+    std::vector<double> nd;
+    double eg;
+    double ndvid;
+    double galutinis;
+    double mediana;
+};
+
+double median(const std::vector<double>& arr);
+void generuotiBalus(std::vector<double>& nd_rezultatai, double& eg);
+void atspausdintiDuomenis(const std::vector<Duomenys>& A, bool iFaila = false);
+void rikiuotiPagalVarda(std::vector<Duomenys>& A);
+void rikiuotiPagalPavarde(std::vector<Duomenys>& A);
+void rikiuotiPagalGalutiniVidurki(std::vector<Duomenys>& A);
+void rikiuotiPagalMediana(std::vector<Duomenys>& A);
+void skaitytiIsFailo(std::vector<Duomenys>& A, const std::string& failoPavadinimas);
+
+#endif // STRUKTURA_H_INCLUDED
 
